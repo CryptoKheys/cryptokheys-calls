83723e961cb39383029eae9f857b42192df8d280
mathieu <mathieu.nicolas@me.com>
2021-04-16 11:39:54 +0200

    colors in calls display

--- src/app/call-card/call-card.component.ts
+++ src/app/call-card/call-card.component.ts
@@ -8,7 +8,7 @@ import {MAT_DIALOG_DATA} from "@angular/material/dialog";
     styleUrls: ['./call-card.component.scss'],
 })
 export class CallCardComponent implements OnInit {
-    @Input() call?: CallDTO | undefined;
+    @Input() call?: CallDTO;
     collapsed: boolean;
 
     constructor(@Inject(MAT_DIALOG_DATA) public data: CallDTO) {
@@ -36,6 +36,36 @@ export class CallCardComponent implements OnInit {
             return Math.floor(counter / 3);
         } else {
             return 0;
+        }
+    }
+
+    pourcentage(): any {
+        if (this.call![2]) {
+            let res = {
+                value: 0,
+                color: '#fff'
+            }
+
+            res.value = (this.call![2].current_price - this.call![1].callPrice) / this.call![1].callPrice
+
+            if (res.value > 0) {
+                res.color = 'bg-green-'
+            } else {
+                res.color = 'bg-red-'
+            }
+
+            let tmp = Math.abs(res.value*10)
+            tmp = Math.floor(tmp)
+            // TODO : Elle pue du cul cette coloration, va falloir reprendre le truc
+            if (tmp > 500) {
+                tmp = 500
+            }
+            if (tmp < 100) {
+                tmp = 100
+            }
+            res.color += '400'
+            res.value *= 100;
+            return res
         }
     }
 }
